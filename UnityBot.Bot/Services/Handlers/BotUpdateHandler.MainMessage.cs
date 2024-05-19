f2de0b33854c50602ded8f455e098ea1a1596535
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-05-19 05:57:49 +0500

    add to partial class

---
+++ UnityBot.Bot/Services/Handlers/BotUpdateHandler.MainMessage.cs
@@ -0,0 +1,48 @@
+﻿using Telegram.Bot.Types;
+using Telegram.Bot;
+
+namespace UnityBot.Bot.Services.Handlers
+{
+    public partial class BotUpdateHandler
+    {
+        private async Task HandleSherikKerakAsync(ITelegramBotClient client, Message message, CancellationToken cancellationToken)
+        {
+            await client.SendTextMessageAsync(
+                chatId: message.Chat.Id,
+                text: "Bu sherik uchun",
+                cancellationToken: cancellationToken);
+        }
+
+        private async Task HandleUstozkerakAsync(ITelegramBotClient client, Message message, CancellationToken cancellationToken)
+        {
+            await client.SendTextMessageAsync(
+                chatId: message.Chat.Id,
+                text: "Bu ustoz uchun",
+                cancellationToken: cancellationToken);
+        }
+
+        private async Task HandleRezumeJoylashAsync(ITelegramBotClient client, Message message, CancellationToken cancellationToken)
+        {
+            await client.SendTextMessageAsync(
+                       chatId: message.Chat.Id,
+                       text: "Bu rezume joylash uchun",
+                       cancellationToken: cancellationToken);
+        }
+
+        private async Task HandleShogirtKerakAsync(ITelegramBotClient client, Message message, CancellationToken cancellationToken)
+        {
+            await client.SendTextMessageAsync(
+                       chatId: message.Chat.Id,
+                       text: "Bu shogirt uchun",
+                       cancellationToken: cancellationToken);
+        }
+
+        private async Task HandleIshJoylashAsync(ITelegramBotClient client, Message message, CancellationToken cancellationToken)
+        {
+            await client.SendTextMessageAsync(
+                       chatId: message.Chat.Id,
+                       text: "Bu ish joylash uchun",
+                       cancellationToken: cancellationToken);
+        }
+    }
+}
